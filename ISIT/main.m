0c6d7202d60c25cc34318d0f22a451823f227448
shengvip <eterou@outlook.com>
2018-01-18 20:42:32 +0800

    Signed-off-by: shengvip <eterou@outlook.com>

--- ISIT/main.m
+++ ISIT/main.m
@@ -1,20 +1,20 @@
-clc;
+﻿clc;
 clear;
-% 初始化参数
-%% **********平面波**************************************
-speaker_array_azimuth=[320,90,95,120]; % 车内音响按四个扬声器试验
+% 鍒濆鍖栧弬鏁�
+%% **********骞抽潰娉�**************************************
+speaker_array_azimuth=[320,90,95,120]; % 杞﹀唴闊冲搷鎸夊洓涓壃澹板櫒璇曢獙
 speaker_array_elevation=[0,0,0,0];
-distance=1; %计算-distance至distance之间的声场。
-F=1000;%计算声压时信号频率
-dB=0;%dB=1，画声压级，dB=0，画声压
+distance=1; %璁＄畻-distance鑷砫istance涔嬮棿鐨勫０鍦恒€�
+F=1000;%璁＄畻澹板帇鏃朵俊鍙烽鐜�
+dB=0;%dB=1锛岀敾澹板帇绾э紝dB=0锛岀敾澹板帇
 
-% 计算声压
+% 璁＄畻澹板帇
 for w1=0:0.1:1
     for w2=0:0.1:1
         for w3=0:0.1:1
             for w4=0:0.1:1
                 w=[w1,w2,w3,w4];
-                if length(speaker_array_azimuth) ~= length(w) % 权重数量与扬声器数量不一致
+                if length(speaker_array_azimuth) ~= length(w) % 鏉冮噸鏁伴噺涓庢壃澹板櫒鏁伴噺涓嶄竴鑷�
                     display('Dimension error. Please check the dimension of w.');
                     break;break;break;break;
                 else
